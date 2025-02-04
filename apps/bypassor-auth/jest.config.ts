be3368ad480300d53e925634be4b9734e1ffa19c
charmingruby <mrrubycanal3105@gmail.com>
2025-02-04 16:37:58 -0300

    feat: base nx project

---
+++ apps/bypassor-auth/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'bypassor-auth',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/bypassor-auth',
+};
